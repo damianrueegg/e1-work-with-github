af6a36fbabc5e7fb4fbfdc0caecde4a9e854e3b6
damianrueegg <38529113+damianrueegg@users.noreply.github.com>
2022-10-17 09:49:58 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
  #!/bin/bash         
 
 echo "Hey, Automation Star!"
-echo "My Name is Damian Rüegg and I work as an Test Automation Engineer. "
+echo "My Name is Damian Rüegg and I work as an Test Automation Engineer in Switzerland. "
 
 var1=${1:-not_set}
 
